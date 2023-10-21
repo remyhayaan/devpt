731be66e9d0079d20cf2ea7b47abf4a65901f1a1
Nabukenya Rehemah <remmiehayaan@gmail.com>
2023-10-22 00:09:58 +0300

    admin page

---
+++ src/components/Admin.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './Header'
+
+function Admin() {
+  return (
+    <div>
+        <Header />
+        admin
+    </div>
+  )
+
+}
+
+export default Admin
